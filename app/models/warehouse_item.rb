36a94dff33f02a3b67b622f7741bb41e39eae619
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-01-10 01:47:41 -0700

    heroku

--- app/models/warehouse_item.rb
+++
@@ -1,4 +0,0 @@
-class WarehouseItem < ApplicationRecord
-  belongs_to :warehouse
-  belongs_to :item
-end
--- spec/models/warehouse_item_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe WarehouseItem do
-  describe 'relationships' do
-    it {should belong_to(:warehouse)}
-    it {should belong_to(:item)}
-  end
-end
