7bb3d6f69571d1511a6ba2e09a4dde065c54bb1c
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-02-07 19:10:33 -0700

    Add destroy action

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -13,11 +13,22 @@ class WarehousesController < ApplicationController
 
   def create
     warehouse = Warehouse.create(warehouse_params)
+
     if warehouse.save
       redirect_to warehouses_path
+      flash[:notice] = "#{warehouse.location} warehouse added."
     else
       redirect_to new_warehouse_path
+      flash[:error] = "#{warehouse.location} warehouse already exists."
     end
+  end
+
+  def destroy
+    warehouse = Warehouse.find(params[:id])
+    
+    warehouse.destroy
+    redirect_to warehouses_path
+    flash[:notice] = "#{warehouse.location} warehouse removed."
   end
 
   private
