a69e4cde0be7e61279ff7a42848eea3994549c94
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-02-08 17:43:20 -0700

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-warehouse_1 = Warehouse.create(location: 'Denver')
+warehouse_1 = Warehouse.create(location: 'Atlanta')
 warehouse_1.items.create(name: 'Shirt', quantity: 20, price: 18)
 warehouse_1.items.create(name: 'Pants', quantity: 30, price: 45)
 
-warehouse_2 = Warehouse.create(location: 'Seattle')
+warehouse_2 = Warehouse.create(location: 'Chicago')
 warehouse_2.items.create(name: 'Ball Cap', quantity: 11, price: 25)
 warehouse_2.items.create(name: 'Socks', quantity: 40, price: 10)
 warehouse_2.items.create(name: 'Watch', quantity: 5, price: 100)
